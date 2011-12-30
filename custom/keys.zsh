265b542f2009eb70e083e4112ad0552f858c859c
Sean Lee <iseansay@gmail.com>
2011-12-30 23:17:24 +0800

    change the public key file path

--- custom/keys.zsh
+++ custom/keys.zsh
@@ -1,2 +1,2 @@
 # Pipe my public key to my clipboard. 
-alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_dsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
