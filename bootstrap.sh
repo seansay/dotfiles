c56cadedc532a6437e9bfca18bcd513a7ad19fc8
Sean Lee <iseansay@gmail.com>
2012-04-10 13:29:40 +0800

    add git-flow to plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@ export EDITOR="emacs"
 export PATH=$PATH:~/bin
 ZSH_THEME="xiong-chiamiov-plus"
 
-plugins=(textmate osx git github brew pip django extract autojump)
+plugins=(textmate osx git github git-flow brew pip django extract autojump)
 
 source $ZSH/oh-my-zsh.sh
