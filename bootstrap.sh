d877b6f6a87d442862aabc8d4ba931820fc0d8bd
Sean Lee <iseansay@gmail.com>
2012-06-02 17:48:35 +0800

    add chrome stylesheet and some functions from https://github.com/mathiasbynens/dotfiles/blob/master/.functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 export EDITOR="emacs"
 export PATH=$PATH:~/bin
+
 ZSH_THEME="xiong-chiamiov-plus"
 
-plugins=(textmate osx git github git-flow brew pip django extract autojump)
+plugins=(textmate osx git github git-flow brew pip django extract autojump rails3 rails gem)
 
 source $ZSH/oh-my-zsh.sh
--- custom/virtualenvwrapper.zsh
+++
@@ -1,6 +0,0 @@
-# use --no-site-packages by default
-export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
-
-
-# simple alias for activate
-alias activate='source venv/bin/activate'
