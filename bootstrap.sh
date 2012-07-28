80692c9adad8c2b61aa8470109afef2f9db00a0b
Sean Lee <iseansay@gmail.com>
2012-07-28 09:47:37 +0800

    added 2 plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@ export PATH=$PATH:~/bin
 
 ZSH_THEME="xiong-chiamiov-plus"
 
-plugins=(textmate osx git github git-flow brew pip django extract autojump rails3 rails gem)
+plugins=(textmate osx git github git-flow brew pip django extract autojump rails3 rails gem encode64 rvm)
 
 source $ZSH/oh-my-zsh.sh
