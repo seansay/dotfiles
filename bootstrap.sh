a4c7f016fd7c153857e8a8ca2610dd70cc21b052
Sean Lee <iseansay@gmail.com>
2013-04-14 11:15:32 +0800

    disbale auto update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,15 @@
-ZSH=$(dirname $0) 
+ZSH=$(dirname $0)
 
 export EDITOR="emacs"
 export PATH=$PATH:~/bin:$ZSH/bin
 
-plugins=(textmate osx git github git-flow brew pip django extract autojump encode64 gem rails3 rake lein cake rvm ruby bundler vagrant zsh-syntax-highlighting)
+plugins=(safe-paste textmate osx git github git-flow brew pip django extract autojump encode64 gem rails3 rake lein cake rvm ruby bundler vagrant zsh-syntax-highlighting)
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 
 ZSH_THEME="sean"
 
-source $ZSH/oh-my-zsh.sh
+# disable auto update
+export DISABLE_AUTO_UPDATE="true"
 
+source $ZSH/oh-my-zsh.sh
