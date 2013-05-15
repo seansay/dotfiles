50e4b6a4152c5f730f0839601c41ce5e7a971ad9
Sean Lee <iseansay@gmail.com>
2013-05-15 20:35:54 +0800

    golang

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ ZSH=$(dirname $0)
 export EDITOR="emacs"
 export PATH=$PATH:~/bin:$ZSH/bin
 
-plugins=(safe-paste textmate osx git github git-flow brew pip django extract autojump encode64 gem rails3 rake lein cake rvm ruby bundler vagrant zsh-syntax-highlighting)
+plugins=(safe-paste textmate osx git github git-flow brew pip django extract autojump encode64 gem rails3 rake lein cake rvm ruby bundler vagrant zsh-syntax-highlighting golang)
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 
